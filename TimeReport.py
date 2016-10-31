a7ca10b48db921af3319aebc926c09287340c81e
Aljon Preza <aljonpreza@yahoo.com>
2016-10-31 12:22:55 -1000

    Changed Comments for Interns Args

--- TimeReport.py
+++ TimeReport.py
@@ -215,7 +215,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Calculate time reports.")
 	parser.add_argument("--date", help="mm.dd.yyyy of date to gather")
 	parser.add_argument("--ignore", help="comma seperated list of people to ignore")
-	parser.add_argument("--interns", help="Use JSON format as arguments for the intern's schedule. Example: {\"Aljon Preza\": [0,1,2]}")
+	parser.add_argument("--interns", help="Use JSON format as arguments for the intern's schedule. Example: {\"Aljon Preza\": [\"M\",\"T\"]}")
 	args = parser.parse_args()
 
         internArgs = None;
